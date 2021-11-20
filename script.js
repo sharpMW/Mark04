2e15fb4346828201812d0ac4c4dcf84b721f2904
sharpMW <88919653+sharpMW@users.noreply.github.com>
2021-11-20 11:08:03 +0530

    menu button added

---
+++ script.js
@@ -0,0 +1,18 @@
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+// const menuButton = document.querySelector(".dropdown");
+// menuButton.addEventListener('click', myFunction);
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+  
